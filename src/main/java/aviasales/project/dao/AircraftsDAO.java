280776dc6f58b60a4be8d809946ac41e5f98f399
YelzhasTaubay <elzhas.96@mail.ru>
2022-07-22 17:25:12 +0600

    Dao

--- src/main/java/aviasales/project/dao/AircraftsDAO.java
+++ src/main/java/aviasales/project/dao/AircraftsDAO.java
@@ -1,4 +1,9 @@
 package aviasales.project.dao;
 
+import aviasales.project.entities.Aircrafts;
+
 public interface AircraftsDAO {
+
+    void addAircraft(Aircrafts aircraft);
+
 }
--- src/main/java/aviasales/project/dao/AircraftsDAOImpl.java
+++ src/main/java/aviasales/project/dao/AircraftsDAOImpl.java
@@ -1,4 +1,24 @@
 package aviasales.project.dao;
 
+import aviasales.project.entities.Aircrafts;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AircraftsDAOImpl implements AircraftsDAO{
+
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public void addAircraft(Aircrafts aircraft) {
+        Session session=sessionFactory.openSession();
+        Transaction transaction=session.beginTransaction();
+        session.save(aircraft);
+        transaction.commit();
+        session.close();
+    }
+
 }
